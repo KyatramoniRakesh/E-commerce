8dd3735fc5e27275d24104b470f93d7af79cb3dc
KyatramoniRakesh <rakesh.rk1306@gmail.com>
2024-03-20 14:18:00 +0530

    first commit

--- EcApp/views.py
+++ EcApp/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Product
+from django.db.models import Count
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+class Category(View):
+    def get(self,request,val):
+        product = Product.objects.filter(category = val)
+        title = Product.objects.filter(category = val).values('title').annotate(total=Count('title'))
+        return render(request,'category.html',locals())
