3f0386b697f4d57e9ef4dddf9c949cb43cdef913
KyatramoniRakesh <rakesh.rk1306@gmail.com>
2024-03-21 00:05:34 +0530

    New commit

--- EcApp/url.py
+++ EcApp/url.py
@@ -4,6 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.home),
+    path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
     path('category/<slug:val>', views.Category.as_view(),name='category'),
+    path('product_details/<int:pk>', views.Product_Details.as_view(), name='productdetails'),
+    path('category-title/<val>', views.CategoryTitle.as_view(), name='category-title'),
+
+    #login authentication
+    path('registration',views.CustomerRegistration.as_view(),name='registration')
 ]+static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
--- EcApp/views.py
+++ EcApp/views.py
@@ -2,12 +2,33 @@ from django.shortcuts import render
 from django.views import View
 from .models import Product
 from django.db.models import Count
+from .forms import Registration
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return render(request,'contact.html')
 
 class Category(View):
     def get(self,request,val):
         product = Product.objects.filter(category = val)
         title = Product.objects.filter(category = val).values('title').annotate(total=Count('title'))
         return render(request,'category.html',locals())
+
+class CategoryTitle(View):
+    def get(self,request,val):
+        product = Product.objects.filter(title = val)
+        title = Product.objects.filter(category = product[0].category).values('title')
+        return render(request,'category.html',locals())
+
+class Product_Details(View):
+    def get(self,request,pk):
+        product = Product.objects.get(pk=pk)
+        return render(request,'ProductDetails.html',{'product': product})
+
+class CustomerRegistration(View):
+    def get(self,request):
+        form = Registration()
+        return render(request,'registration.html',locals())
