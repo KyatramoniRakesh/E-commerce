3903af788aa97d1b24574c58162c0a5dd23d65c5
KyatramoniRakesh <rakesh.rk1306@gmail.com>
2024-03-23 15:33:27 +0530

    Project in Process

--- EcApp/url.py
+++ EcApp/url.py
@@ -16,6 +16,12 @@ urlpatterns = [
     path('address', views.address, name='address'),
     path('updateaddress/<int:pk>',views.UpdateAddress.as_view(),name='update_add'),
 
+    #Add_to_cart
+    path('add',views.add_to_cart, name='add'),
+    path('cart',views.show_cart, name='cart'),
+    path('checkout',views.show_cart, name='checkout'),
+
+
     #login authentication
     path('registration',views.CustomerRegistration.as_view(),name='registration'),
     path('accounts/login',auth_view.LoginView.as_view(template_name = 'Login.html',authentication_form = Login), name='login'),
@@ -23,19 +29,12 @@ urlpatterns = [
     path('passwordchange',auth_view.PasswordChangeView.as_view(template_name='changepassword.html',
                 form_class = MyPasswordChangeForm,success_url='passwordchangedone'),name='pc'),
     path('logout',auth_view.LogoutView.as_view(next_page='login'),name='logout'),
-
     path('password-reset/',auth_view.PasswordResetView.as_view(template_name='password_reset.html',
     form_class=MyPasswordResetForm), name='password_reset'),
-
     path('password-reset/done/', auth_view.PasswordResetDoneView.as_view(template_name='password_reset_done.html') ,
     name='password_reset_done'),
-
     path('password-reset-confirm/<uidb64>/<token>/',auth_view.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html',
                                             form_class=MySetPasswordForm),name='password_reset_confirm'),
-
     path('password-reset-complete/', auth_view.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
         name='password_reset_complete'),
-
-
-
     ]+static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
--- EcApp/views.py
+++ EcApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.views import View
-from .models import Product
+from .models import Product,Customer,Cart
 from django.db.models import Count
 from .forms import Registration,CustomerProfileForm
 from django.contrib import messages
@@ -91,3 +91,18 @@ class UpdateAddress(View):
             messages.warning(request, "Invalid Input Data")
         return redirect("address")
 
+def add_to_cart(request):
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user,product=product).save()
+    return redirect("cart")
+def show_cart(request):
+    user = request.user
+    cart = Cart.objects.filter(user=user)
+    amount = 0
+    for p in cart:
+        value = p.quantity * p.product.discounted_price
+        amount += value
+        totalamount = amount + 40
+    return render(request,'addtocart.html',locals())
