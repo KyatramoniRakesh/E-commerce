bec1f8b322cceb14a895f8570c7424ec9c07068a
KyatramoniRakesh <rakesh.rk1306@gmail.com>
2024-03-31 12:27:40 +0530

    commit

--- EC/settings.py
+++ EC/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-0#x5o^+a%n@pf0=h+2z%yd1_ivy9qs-ru$*^pd7mg(g^m$p(up
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
